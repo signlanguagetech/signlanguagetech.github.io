4beff1fb12339200264378ca7b46b00a462e565d
Milad Vafaeifard <milad.vafaeifard@gmail.com>
2024-05-21 13:44:44 +0200

    Update title and description globall

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Sign Language Tech';
+export const SITE_DESCRIPTION = 'Welcome to Sign Language Tech!';
