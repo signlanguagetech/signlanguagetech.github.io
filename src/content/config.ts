8ba62dfbc62dee752846907ec26945408c2b5aff
Milad Vafaeifard <milad.vafaeifard@gmail.com>
2024-05-26 17:02:47 +0200

    init talks

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,9 +9,22 @@ const vlogs = defineCollection({
     // Transform string to Date object
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
+    heroImage: z.string(),
+  }),
+});
+
+const talks = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    speaker: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    thumbnail: z.string(),
+    alt: z.string().optional()
   }),
 });
 
 
-export const collections = { vlogs };
+export const collections = { vlogs, talks };
