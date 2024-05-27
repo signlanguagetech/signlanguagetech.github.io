bd0ec28122ca979b2a07c9a9d47e0c061e9cf613
Milad Vafaeifard <milad.vafaeifard@gmail.com>
2024-05-28 01:01:22 +0200

    Refactor collection definition

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,31 +1,26 @@
 import { defineCollection, z } from 'astro:content';
 
-const vlogCollection = defineCollection({
-  type: "content",
-  // Type-check frontmatter using a schema
-  schema: z.object({
+const videoCollectionSchemaBase = z.object({
     title: z.string(),
     description: z.string(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string(),
-  }),
-});
-
-const talkCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    speaker: z.string(),
+    speaker: z.string().optional(),
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
     thumbnail: z.string(),
     alt: z.string().optional()
-  }),
+  });
+
+export type VideoCollectionSchemaBase = typeof videoCollectionSchemaBase;
+
+const vlogCollection = defineCollection({
+  type: 'content',
+  schema: videoCollectionSchemaBase,
 });
 
+const talkCollection = defineCollection({
+  type: 'content',
+  schema: videoCollectionSchemaBase,
+});
 
 export const collections = {
   vlogs: vlogCollection,
