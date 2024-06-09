cb0b6bb46accb1a575bde57c0d19e0126c3cd1cc
Milad Vafaeifard <milad.vafaeifard@gmail.com>
2024-06-09 23:58:52 +0200

    Refactor glossary part 2

--- src/helpers/helper.ts
+++ src/helpers/helper.ts
@@ -4,3 +4,7 @@ import type { ContentKeys } from "../model/model";
 
 export const sortDate = <T extends CollectionEntry<ContentKeys>>(a: T, b: T) =>
   b.data.pubDate.valueOf() - a.data.pubDate.valueOf();
+
+export const first = <T>(collections: T[]): T | undefined => {
+  return collections.length === 0? undefined : collections[0];
+}
