b4eba986ba227795eaad181225baaac20708cf0a
Milad Vafaeifard <milad.vafaeifard@gmail.com>
2024-07-03 21:07:26 +0200

    Fix format

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import alpinejs from "@astrojs/alpinejs";
+
 const port = 4300;
 const localHostUrl = `http://localhost:${port}`;
 const liveUrl = 'https://signlanguagetech.com';
