4b5d2d29cf9e6da038838f4237eaa939a7f8ac44
Milad Vafaeifard <milad.vafaeifard@gmail.com>
2024-08-14 22:25:48 +0200

    Update alogia search

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,6 +2,7 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-  readonly PUBLIC_APP_ID: string;
-  readonly PUBLIC_API_KEY: string;
+  readonly ALOGIA_PUBLIC_APP_ID: string;
+  readonly ALOGIA_PUBLIC_API_KEY: string;
+  readonly ALOGIA_INDEX_NAME: string;
 }
